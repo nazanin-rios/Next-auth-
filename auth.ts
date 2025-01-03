fed56d90a753f46ccb11f34625c895ea3cea3729
nazanin-rios <157608782+nazanin-rios@users.noreply.github.com>
2025-01-03 16:06:19 +0330

    add callbacks

--- auth.ts
+++ auth.ts
@@ -1,13 +1,36 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-import authConfig from "./auth.config"
-import { db } from "./lib/db"
- 
-const prisma = new PrismaClient()
- 
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import authConfig from "./auth.config";
+import { db } from "./lib/db";
+import { getUserById } from "./data/user";
+
+
+const prisma = new PrismaClient();
+
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      // console.log({ sessionToken: token, });
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      // console.log({ token });
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      token.role = existingUser.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
-})
+});
